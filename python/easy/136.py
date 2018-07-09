658fb05405284e9032b2043ff7ebbb8a96bfd2a8
Ivan Chen <allenivan@gmail.com>
2018-07-09 17:18:58 +0800

    Update 136.py

--- python/easy/136.py
+++ python/easy/136.py
@@ -8,3 +8,19 @@ class Solution:
         for i in range(1, len(nums)):
             ans^=nums[i]
         return ans
+
+# faster one
+
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        table = {}
+        for num in nums:
+            if num not in table:
+                table[num] = 1
+            else:   table[num] += 1
+        for num in nums:
+            if table[num] == 1: return num
