44cb58034f44d1eeca29d4f96a31725702da537b
Ivan Chen <allenivan@gmail.com>
2018-08-16 23:41:24 +0800

    690

---
+++ python/easy/690.py
@@ -0,0 +1,30 @@
+"""
+# Employee info
+class Employee:
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution:
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        table = {e.id: e for e in employees}
+        stack=[table[id]]
+        ans = 0
+        while len(stack) > 0:
+            top = stack.pop()
+            ans += top.importance
+            for sub in top.subordinates:
+                stack.append(table[sub])
+        return ans
+                       
+            
