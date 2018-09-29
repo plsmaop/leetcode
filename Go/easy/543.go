8f1f20cb7402930b32cd166d40287afcfd2f05d4
Ivan Chen <allenivan@gmail.com>
2018-09-29 20:51:45 +0800

    tree

---
+++ Go/easy/543.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+	ans := 0
+	var dfs func(*TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		if ans < left+right {
+			ans = left + right
+		}
+		if left < right {
+			left = right
+		}
+		return 1 + left
+	}
+	dfs(root)
+	return ans
+}
